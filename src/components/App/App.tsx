ad49dd5a4716044d288628f24872f3c93bbb54bb
motsukov <motsukov@gmail.com>
2023-11-16 18:38:06 +0300

    githab pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,14 @@
-import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AppRoutes from "../../pages";
 import { store } from "../../services";
 
 export const App = () => {
 	return (
 		<Provider store={store}>
-			<BrowserRouter>
+			<HashRouter>
 				<AppRoutes />
-			</BrowserRouter>
+			</HashRouter>
 		</Provider>
 	);
 };
